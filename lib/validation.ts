2ca203211aa29cb5ee8ff8645217b9155b6160b5
morningstar <manhvp1810@gmail.com>
2025-05-28 22:46:03 +0700

    refactor: update validation schemas for working times and time off schedule; simplify hour validation and enhance day uniqueness checks

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,15 +1,10 @@
 import { z } from "zod";
-import { isValidHour } from "./time-picker-utils";
 
 export const workingTimeSchema = z
   .array(
     z.object({
-      from: z.string().refine(isValidHour, {
-        message: "Please enter a valid hour",
-      }),
-      to: z.string().refine(isValidHour, {
-        message: "Please enter a valid hour",
-      }),
+      from: z.string(),
+      to: z.string(),
       day: z.string(),
     })
   )
@@ -17,30 +12,43 @@ export const workingTimeSchema = z
     message: "You have to select at least one day.",
   });
 
-export const timeOffScheduleFormSchema = z.object({
-  date: z.string({
-    required_error: "Please select a date",
-  }),
-  fromTime: z.string({
-    required_error: "Please select a time",
-  }),
-  toTime: z.string({
-    required_error: "Please select a time",
-  }),
-  reason: z.string().min(1, "Please enter a reason"),
-});
+export const timeOffScheduleFormSchema = z.array(
+  z.object({
+    date: z.date().optional(),
+    from: z.string(),
+    to: z.string(),
+    reason: z.string({
+      required_error: "Please enter a reason",
+    }),
+    dayOfWeek: z
+      .array(z.number().min(1).max(7))
+      //check if the days are unique
+      .refine((value) => new Set(value).size === value.length, {
+        message: "Please select unique days",
+      })
+      .optional(),
+    dayOfMonth: z
+      .array(z.number().min(1).max(31))
+      .refine((value) => new Set(value).size === value.length, {
+        message: "Please select unique days",
+      })
+      .optional(),
+    period: z.enum(["Exact", "Daily", "Weekly", "Monthly"]),
+  })
+);
 
 export const employeeFormSchema = z.object({
   firstName: z.string().min(1, "First name is required"),
   lastName: z.string().min(1, "Last name is required"),
-  phone: z.string().min(1, "Phone number is required"),
-  position: z.string().min(1, "Position is required"),
-  startDate: z.date({
-    required_error: "Please select a start date",
+  phone: z
+    .string()
+    .refine(
+      (value) => /^[+]{1}(?:[0-9-()/.]\s?){6,15}[0-9]{1}$/.test(value),
+      "Please enter a valid phone number"
+    ),
+  position: z.string({
+    required_error: "Please select a position",
   }),
-  endDate: z.date().optional(),
   workingTimes: workingTimeSchema,
   timeOffSchedule: timeOffScheduleFormSchema.optional(),
 });
-
-export type EmployeeFormValues = z.infer<typeof employeeFormSchema>;
