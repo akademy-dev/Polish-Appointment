8cd8d05f267e845bf9baadbae25a2375faed7db4
morningstar <manhvp1810@gmail.com>
2025-05-31 18:00:37 +0700

    feat: add Sanity schema definitions for working time, time-off schedule, customer, employee, service, and category; generate TypeScript types and update structure for improved content management

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,12 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Content")
+    .items([
+      S.documentTypeListItem("employee").title("Employees"),
+      S.documentTypeListItem("customer").title("Customers"),
+      S.documentTypeListItem("service").title("Services"),
+      S.documentTypeListItem("category").title("Categories"),
+    ]);
