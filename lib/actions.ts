48b239e53f67d354d1b08923654f8c5042b2b11e
morningstar <manhvp1810@gmail.com>
2025-06-01 15:08:48 +0700

    feat: implement update and create functionality for customers and employees

--- lib/actions.ts
+++ lib/actions.ts
@@ -41,3 +41,110 @@ export const createEmployee = async (
     });
   }
 };
+
+export const updateEmployee = async (
+  _id: string,
+  form: FormData,
+  workingTimes: WorkingTime[],
+  timeOffSchedule: TimeOffSchedule[]
+) => {
+  const { firstName, lastName, phone, position } = Object.fromEntries(
+    Array.from(form)
+  );
+
+  try {
+    const employee = {
+      firstName,
+      lastName,
+      phone,
+      position,
+      workingTimes,
+      timeOffSchedule,
+    };
+
+    const result = await writeClient
+      .patch(_id)
+      .set({
+        ...employee,
+      })
+      .commit();
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.log(error);
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
+};
+
+export const createCustomer = async (form: FormData) => {
+  const { firstName, lastName, phone, email } = Object.fromEntries(
+    Array.from(form)
+  );
+
+  try {
+    const customer = {
+      firstName,
+      lastName,
+      phone,
+      email,
+    };
+
+    const result = await writeClient.create({
+      _type: "customer",
+      ...customer,
+    });
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.log(error);
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
+};
+
+export const updateCustomer = async (_id: string, form: FormData) => {
+  const { firstName, lastName, phone, email } = Object.fromEntries(
+    Array.from(form)
+  );
+
+  try {
+    const customer = {
+      firstName,
+      lastName,
+      phone,
+      email,
+    };
+
+    const result = await writeClient
+      .patch(_id)
+      .set({
+        ...customer,
+      })
+      .commit();
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.log(error);
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
+};
