993d46248dcc43f60e979d0c5d7048e6f0af45a3
AmbroseNTK <kiettuannguyense@gmail.com>
2025-11-18 23:17:41 +0700

    refactor: format Cache-Control header value for improved readability in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,8 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+            value:
+              "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
           },
           {
             key: "Pragma",
